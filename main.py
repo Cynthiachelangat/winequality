d1ea42b8e5ab77305f072b9221f2902890f28460
Cynthiachelangat <cchela789@gmail.com>
2024-10-31 17:15:22 +0300

    Data Transformation

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from winequality import logger
 from winequality.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from winequality.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
-# from winequality.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+from winequality.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 # from winequality.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
 # from winequality.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
 
@@ -21,6 +21,17 @@ STAGE_NAME = "Data Validation stage"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_ingestion = DataValidationTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+STAGE_NAME = "Data Transformation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataTransformationTrainingPipeline()
    data_ingestion.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
