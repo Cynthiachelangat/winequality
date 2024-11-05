3b40b06f5b1a979e21c5a41d57c915ecc30ee03d
Cynthiachelangat <cchela789@gmail.com>
2024-11-05 09:26:47 +0300

    Updating components & pipelining model evaluation

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from winequality.pipeline.stage_01_data_ingestion import DataIngestionTrainingPi
 from winequality.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from winequality.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 from winequality.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
-# from winequality.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
+from winequality.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
 
 
 STAGE_NAME = "Data Ingestion stage"
@@ -42,6 +42,16 @@ STAGE_NAME = "Model Trainer stage"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_ingestion = ModelTrainerTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+STAGE_NAME = "Model evaluation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = ModelEvaluationTrainingPipeline()
    data_ingestion.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
